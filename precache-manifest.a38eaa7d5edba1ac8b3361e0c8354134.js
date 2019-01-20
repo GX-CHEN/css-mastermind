d859b3b49cebabafdfd9b675ec4dd332f2924079
Gongxia Chen <chengongxia1990@gmail.com>
2019-01-20 04:16:37 -0500

    Updates

---
+++ precache-manifest.a38eaa7d5edba1ac8b3361e0c8354134.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7304550a45fadc74d5d9",
+    "url": "/css-mastermind/static/js/runtime~main.7304550a.js"
+  },
+  {
+    "revision": "c81b6bba49cd5619ba52",
+    "url": "/css-mastermind/static/js/main.c81b6bba.chunk.js"
+  },
+  {
+    "revision": "6859f4e6ec934ee81cb3",
+    "url": "/css-mastermind/static/js/1.6859f4e6.chunk.js"
+  },
+  {
+    "revision": "c81b6bba49cd5619ba52",
+    "url": "/css-mastermind/static/css/main.34ac973b.chunk.css"
+  },
+  {
+    "revision": "3739ec0adf6af201edb2303a4277add2",
+    "url": "/css-mastermind/index.html"
+  }
+];
