ec1385e9cabd8758c01473742dc1e31b90331371
Gongxia Chen <chengongxia1990@gmail.com>
2019-01-20 04:25:41 -0500

    use new data to test repl.it css embedded view

---
+++ build/precache-manifest.56962cc660fd87a074dc449ee5c4dc24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7304550a45fadc74d5d9",
+    "url": "/css-mastermind/static/js/runtime~main.7304550a.js"
+  },
+  {
+    "revision": "27998e5a0adbf0be4314",
+    "url": "/css-mastermind/static/js/main.27998e5a.chunk.js"
+  },
+  {
+    "revision": "6859f4e6ec934ee81cb3",
+    "url": "/css-mastermind/static/js/1.6859f4e6.chunk.js"
+  },
+  {
+    "revision": "27998e5a0adbf0be4314",
+    "url": "/css-mastermind/static/css/main.34ac973b.chunk.css"
+  },
+  {
+    "revision": "f418d7645c3ac07e708b8edebe83864c",
+    "url": "/css-mastermind/index.html"
+  }
+];
